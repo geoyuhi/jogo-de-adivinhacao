fa62e1630bab3edb8f6d7978cc4b493ed26b86fe
geovanna <gegerochaaraujo@gmail.com>
2025-03-20 20:07:59 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo de Adivinhação.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Jogo_de_Adivinhação.ConsoleApp
+{
+    internal class Program
+    {
+        //Versao 1 : Estrutura Básica e entrada do usuario
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("-------------------------");
+
+            // logica do jogo
+            Console.WriteLine("dIgite um número:");
+            int numerodigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você digitou o número " + numerodigitado);
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
